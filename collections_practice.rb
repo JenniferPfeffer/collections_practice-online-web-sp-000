5dd9610942b671566cd431e28a9218d8b2e55f36
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-28 18:57:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,10 +5,10 @@ def sort_array_desc(array)
   array.sort.reverse
 end 
 def sort_array_char_count(array) 
-  array.sort.each do |string| 
-    string.length
+  array.sort do |left, right|
+    left.length <=> right.length
   end
-end 
+end
 def swap_elements(array) 
   array[1], array[2] = array[2], array[1] 
   array
